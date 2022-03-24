baa4985d63d91221f964cdef9467d2e91c5f2d32
Maciej Kamianowski <kamianowskimaciej@gmail.com>
2022-03-24 21:44:43 +0100

    Initialize spring boot project and created Post entity class

---
+++ src/main/java/com/kamianowski/maciej/blog/entity/Post.java
@@ -0,0 +1,39 @@
+package com.kamianowski.maciej.blog.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(
+        name = "post", uniqueConstraints = {@UniqueConstraint(columnNames = {"title"})}
+)
+public class Post {
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    private Long id;
+
+    @Column(
+            name = "title", nullable = false
+    )
+    private String title;
+
+    @Column(
+            name = "description", nullable = false
+    )
+    private String description;
+
+    @Column(
+            name = "content", nullable = false
+    )
+    private String content;
+}
