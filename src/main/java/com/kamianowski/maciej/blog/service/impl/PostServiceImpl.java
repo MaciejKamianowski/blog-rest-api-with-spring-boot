cee021bd1f6f1fad1465e7d3ff2071fb0271d306
Maciej Kamianowski <kamianowskimaciej@gmail.com>
2022-03-26 16:49:58 +0100

    Added convertDtoToEntity() method.

--- src/main/java/com/kamianowski/maciej/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/kamianowski/maciej/blog/service/impl/PostServiceImpl.java
@@ -6,9 +6,6 @@ import com.kamianowski.maciej.blog.repository.PostRepository;
 import com.kamianowski.maciej.blog.service.PostService;
 import org.springframework.stereotype.Service;
 
-import java.lang.reflect.Field;
-import java.util.Arrays;
-
 @Service
 public class PostServiceImpl implements PostService {
 
@@ -22,15 +19,10 @@ public class PostServiceImpl implements PostService {
     public PostDto createPost(PostDto dto) {
 
         // convert Dto to entity
-        Post post = new Post();
-        post.setId(dto.getId());
-        post.setContent(dto.getContent());
-        post.setDescription(dto.getDescription());
-        Post newPost = postRepository.save(post);
+        Post newPost = postRepository.save(dto.convertDtoToEntity());
 
         // convert Entity to dto
-//        PostDto postResponse = new PostDto();
-        PostDto postResponse = post.convertEntityToDto();
+        PostDto postResponse = newPost.convertEntityToDto();
         return postResponse;
     }
 
