f638829281ce73d186961ef2f80006d6e05588e0
Maciej Kamianowski <kamianowskimaciej@gmail.com>
2022-04-02 15:45:39 +0200

    Extracted constants into AppConstants class

--- src/main/java/com/kamianowski/maciej/blog/controller/PostController.java
+++ src/main/java/com/kamianowski/maciej/blog/controller/PostController.java
@@ -3,11 +3,10 @@ package com.kamianowski.maciej.blog.controller;
 import com.kamianowski.maciej.blog.payload.PostDto;
 import com.kamianowski.maciej.blog.payload.PostResponse;
 import com.kamianowski.maciej.blog.service.PostService;
+import com.kamianowski.maciej.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -28,17 +27,17 @@ public class PostController {
     // get all posts
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) Integer pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) Integer pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+            @RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) Integer pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) Integer pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ) {
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
     // get post by id
     @GetMapping("/{id}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id ) {
+    public ResponseEntity<PostDto> getPostById(@PathVariable(name = "id") Long id) {
         return ResponseEntity.ok(postService.getPostById(id));
     }
 
