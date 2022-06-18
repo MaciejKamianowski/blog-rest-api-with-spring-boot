e20e7e5a9cce0002d24638b6f4ad2efa695ca4c3
Maciej Kamianowski <kamianowskimaciej@gmail.com>
2022-06-18 20:02:45 +0200

    Resolved handling BlogAPIException

--- src/main/java/com/kamianowski/maciej/blog/payload/GlobalExceptionHandler.java
+++ src/main/java/com/kamianowski/maciej/blog/payload/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.kamianowski.maciej.blog.payload;
 
+import com.kamianowski.maciej.blog.exception.BlogAPIException;
 import com.kamianowski.maciej.blog.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,15 @@ public class GlobalExceptionHandler {
                 exception.getMessage(),
                 request.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleBlogAPIException(BlogAPIException exception,
+                                                                        WebRequest request) {
+        ErrorDetails errorDetails = new ErrorDetails(new Date(),
+                exception.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
     // global exceptions
 
