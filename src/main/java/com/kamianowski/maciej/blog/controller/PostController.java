be3967c994cbbd96a1814ad0bdcd01b73498de9c
Maciej Kamianowski <kamianowskimaciej@gmail.com>
2022-06-25 20:26:33 +0200

    Secured API methods (POST, PUT, GET, DELETE) with in-memory authentication based on user's role

--- src/main/java/com/kamianowski/maciej/blog/controller/PostController.java
+++ src/main/java/com/kamianowski/maciej/blog/controller/PostController.java
@@ -6,6 +6,7 @@ import com.kamianowski.maciej.blog.service.PostService;
 import com.kamianowski.maciej.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -21,6 +22,7 @@ public class PostController {
     }
 
     // create blog post
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto dto) {
         return new ResponseEntity<>(postService.createPost(dto), HttpStatus.CREATED);
@@ -44,11 +46,13 @@ public class PostController {
     }
 
     // update post by id
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto dto, @PathVariable(name = "id") Long id) {
         return new ResponseEntity<>(postService.updatePost(dto, id), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePost(@PathVariable(name = "id") Long id) {
         postService.deletePostById(id);
